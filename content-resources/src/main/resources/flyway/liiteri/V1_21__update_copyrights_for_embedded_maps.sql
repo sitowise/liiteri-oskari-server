a26f4b2c8b291b037588c2de2100fd4d968b44e2
Krzysztof Kozlowski <krzysztof.kozlowski@sitopoland.com>
2020-02-05 12:39:17 +0100

    add copyrights link to embedded maps

---
+++ content-resources/src/main/resources/flyway/liiteri/V1_21__update_copyrights_for_embedded_maps.sql
@@ -0,0 +1,5 @@
+UPDATE portti_view_bundle_seq SET config = '
+{
+    "termsOfUseUrl" : "http://www.ymparisto.fi/download/noname/%7B8183269B-E542-446E-A406-E59A3D5484C9%7D/136329"
+}'
+WHERE bundleinstance = 'publisher2';
